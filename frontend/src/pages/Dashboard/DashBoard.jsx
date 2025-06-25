1b77404c1e01b6a19798485f6db0da574b36199d
abdulkalam0018 <abdulkalam754600@gmail.com>
2025-06-25 15:04:55 +0530

    Implement routing and add Dashboard component with initial layout and styles

---
+++ frontend/src/pages/Dashboard/DashBoard.jsx
@@ -0,0 +1,32 @@
+import react from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./dashboard.css"; // Assuming you have a CSS file for styling
+const DashBoard = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="dashboard-container">
+            <h1>
+                Welcome to the RoadResQ
+            </h1>
+            <div className="dashboard-buttons">
+                <button className="dashboard-btn blue" onClick={() => navigate('/report')}>
+                    Report Vechile breakdown
+                </button>
+
+                <button className="dashboard-btn green" onClick={() => navigate('/chat')}>
+                    Chat with a Mechanic
+                </button>
+                <button className="dashboard-btn yellow" onClick={() => navigate('/track')}>
+                    Track your request
+                </button>
+                <button className="dashboard-btn red" onClick={() =>{localStorage.clear(); navigate('/')}}>
+                    Logout
+                </button>
+
+
+            </div>
+        </div>
+    );
+};
+export default DashBoard;
